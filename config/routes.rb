1d10603a71dcdb377ed7c9d2a1c07ba473feffa9
Yuxin Zhang <Yuxinzhang2024@u.northwestern.edu>
2024-05-03 16:25:51 +0000

    1

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   # get("/things", :controller => "things", :action => "index")
   get("/tacos", :controller => "tacos", :action =>"index")
   get("/dice", :controller => "dice", :action =>"index")
+  get("/company", :controller => "company", :action =>"index")
+  get("/contacts", :controller => "contacts", :action =>"index")
 end
